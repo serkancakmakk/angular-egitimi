52eba0e400ffba36d6408d7a83e0585518c04fda
Serkan <cakmakserkan07@gmail.com>
2025-04-04 16:57:26 +0300

    angular-egitim-if-else-switch-case

--- sart-bloklari/src/app/app.component.ts
+++ sart-bloklari/src/app/app.component.ts
@@ -4,9 +4,51 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template:`
+  <h1>Şart Blokları</h1>
+<!-- 
+  Angular 17 ile değişmiş
+  <p *ngIf ="firstWord" style="color:red;">Birinci Yazı</p>
+  <p *ngIf ="firstWord" style="color:green;">İkinci Yazı</p>
+   -->
+@if(firstWord){
+  <p style="color:red;">Birinci Yazı</p>
+  <p style="color:{{firstWord === true ? 'red': ''}}">Birinci Yazı {{firstWord === true ? 'red': ''}}</p>
+}
+@if(secondWord){
+  <p style="color:green;">İkinci Yazı</p>
+}
+<hr>
+  <button (click)="show(1)">Birinci Yazıyı Göster</button>
+  <button (click)="show(2)"> ikinci Yazıyı Göster</button>
+  <button (click)="show(3)"> Tümü Yazıyı Göster</button>
+
+  `
 })
 export class AppComponent {
-  title = 'sart-bloklari';
+  firstWord:boolean=false;
+  secondWord:boolean=false;
+
+  show(num:number){
+    if(num===1){
+      this.firstWord=true;
+      this.secondWord=false;
+    }else if(num==2){
+      this.firstWord=false;
+      this.secondWord=true;
+    }
+    else{
+      this.firstWord=true;
+      this.secondWord=true;
+    }
+    switch(num){
+      case 1:
+        this.firstWord=true;
+        this.secondWord=false;
+        break;
+      case 2:
+        this.firstWord=false;
+        this.secondWord=true;
+    }
+  }
 }
