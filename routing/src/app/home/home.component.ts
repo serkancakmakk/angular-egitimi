f631db08494e38682c4d3c11b1b3b1b85f133156
Serkan <cakmakserkan07@gmail.com>
2025-04-04 17:25:27 +0300

    angular-egitim-fonksiyon-ile-routing

--- routing/src/app/home/home.component.ts
+++ routing/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  constructor(
+    private route:Router
+  ){}
+  about(){
+    this.route.navigate(["about"]) // this.route.navigate(["about","sadsad"])
+    this.route.navigateByUrl("/about") // this.route.navigateByUrl("/about/56544/5544")
+}
 }
